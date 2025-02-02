5db05716172ebc5c1757d0a44515c5f795f8310e
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2025-02-02 18:21:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 
-import React from "react";
-import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router";
+import { lazy } from "react";
+const Home = lazy(() => import("./pages/home"));
+const About = lazy(() => "./pages/about");
+import Team from "./pages/team";
+import Contact from "./pages/contact";
+import Chat from "./pages/chat";
+import NotFound from "./pages/notFound";
+import Inbox from "./pages/inbox";
+import Signin from "./pages/signin";
+import PrivateRoute from "./routes/privateRoute";
+import ProtectedRoute from "./routes/protectedRoute";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/contact" element={<Contact />} />
 
-export default App
+        <Route element={<ProtectedRoute />}>
+          <Route path="/signin" element={<Signin />} />
+        </Route>
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/chat/:userid" element={<Chat />} />
+          <Route path="/inbox" element={<Inbox />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
